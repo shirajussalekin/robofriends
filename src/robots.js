02cc7ed072d378d9c3da31270d269199b139191a
shirajussalekin <salekin.net2008@gmail.com>
2019-12-02 00:04:37 +0600

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Atiqur Rahman',
+        username: 'Atiq',
+        email: 'atiq@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Sakib Al Hasan',
+        username: 'Sakib',
+        email: 'sakib@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Tamim Iqbal',
+        username: 'Tamim',
+        email: 'tamim@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Mushfiqur Rahman',
+        username: 'Mushfiq',
+        email: 'mushfiq@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Mashrafee Martuza',
+        username: 'Mash',
+        email: 'mash@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Imrul Kayes',
+        username: 'Imrul',
+        email: 'imrul@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Abdur Razzak',
+        username: 'Razzak',
+        email: 'razzak@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Rubel Hossain',
+        username: 'Rubel',
+        email: 'rubel@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Sahadat Hossain',
+        username: 'Sahadat',
+        email: 'sahadat@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Mustafiz Rahman',
+        username: 'Mustafiz',
+        email: 'mustafiz@gmail.com'
+    },
+]
